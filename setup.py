7b5c4e21bf99f06ab7a7f19ee67483677a05b5c8
Evgeny Ermakov <e.ermakov@corp.mail.ru>
2017-09-04 17:57:30 +0300

    package setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'MABpy',
+  packages = ['MABpy'], # this must be the same as the name above
+  version = '0.1-alpha',
+  description = 'The multi-armed bandit problem models library and various strategies analysis.',
+  author = 'iJKos',
+  author_email = 'JK.Ermakov@gmail.com',
+  url = 'https://github.com/iJKos/MABpy', # use the URL to the github repo
+  download_url = 'https://github.com/iJKos/MABpy/releases/tag/v0.1-alpha',
+  keywords = ['MultiArmedBandits', 'ReinforsmentLearning'], # arbitrary keywords
+  classifiers = [],
+)
